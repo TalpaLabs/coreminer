d50c28554dedc3c43e073b211e79899eef4ab3c6
Christoph J. Scherr <software@cscherr.de>
2025-02-26 16:57:46 +0100

    docs: document feedback module

--- src/feedback.rs
+++ src/feedback.rs
@@ -1,3 +1,16 @@
+//! # Feedback Module
+//!
+//! Provides types for communicating between the debugger and user interface.
+//!
+//! This module defines the [`Feedback`] enum, which is used to represent
+//! the results of debugging operations in a structured way that can be
+//! presented to the user. It serves as the primary communication channel
+//! between the debugger core and the user interface.
+//!
+//! The different variants of the [`Feedback`] enum represent different types
+//! of information that might be returned from debugging operations, such as
+//! register values, memory contents, disassembly, and error conditions.
+
 use std::fmt::Display;
 
 use nix::libc::user_regs_struct;
@@ -9,20 +22,78 @@ use crate::unwind::Backtrace;
 use crate::variable::VariableValue;
 use crate::{Addr, Word};
 
+/// Represents the result of a debugging operation
+///
+/// [`Feedback`] is used to communicate the results of debugging operations
+/// between the debugger core and the user interface. Each variant represents
+/// a different type of result that might be returned from a debugging operation.
+///
+/// # Examples
+///
+/// ```no_run
+/// use coreminer::feedback::Feedback;
+/// use coreminer::addr::Addr;
+///
+/// // Function that might return different types of feedback
+/// fn example_operation(operation: &str) -> Feedback {
+///     match operation {
+///         "read_mem" => Feedback::Word(42),
+///         "get_addr" => Feedback::Addr(Addr::from(0x1000usize)),
+///         "error" => Feedback::Error(coreminer::errors::DebuggerError::NoDebugee),
+///         _ => Feedback::Text(format!("Unknown operation: {}", operation)),
+///     }
+/// }
+///
+/// // Processing feedback in a UI
+/// fn display_feedback(feedback: Feedback) {
+///     match feedback {
+///         Feedback::Word(word) => println!("Word value: {:#x}", word),
+///         Feedback::Addr(addr) => println!("Address: {}", addr),
+///         Feedback::Error(err) => println!("Error: {}", err),
+///         Feedback::Text(text) => println!("{}", text),
+///         _ => println!("Other feedback type: {}", feedback),
+///     }
+/// }
+/// ```
 #[derive(Debug)]
 pub enum Feedback {
+    /// Simple text message
     Text(String),
+
+    /// Memory word value
     Word(Word),
+
+    /// Memory address
     Addr(Addr),
+
+    /// Register values
     Registers(user_regs_struct),
+
+    /// Error condition
     Error(DebuggerError),
+
+    /// Success with no specific data
     Ok,
+
+    /// Disassembled code
     Disassembly(Disassembly),
+
+    /// Call stack backtrace
     Backtrace(Backtrace),
+
+    /// Debug symbols
     Symbols(Vec<OwnedSymbol>),
+
+    /// Variable value
     Variable(VariableValue),
+
+    /// Stack contents
     Stack(crate::stack::Stack),
+
+    /// Process memory map
     ProcessMap(Vec<proc_maps::MapRange>),
+
+    /// Debuggee process exit
     Exit(i32),
 }
 

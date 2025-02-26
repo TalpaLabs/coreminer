c3378cc685d151b76623ed4b7e3442d3dfbd3d83
Christoph J. Scherr <software@cscherr.de>
2025-02-26 15:11:52 +0100

    chore: fix build-release script

--- build-release.sh
+++ build-release.sh
@@ -5,7 +5,7 @@ D2=./target/release/dummy2
 D3=./target/release/dummy3
 gcc ./examples/dummy.c --output $D1
 gcc ./examples/dummy2.c --output $D2
-gcc ./examples/dummy3.c --output $D2
+gcc ./examples/dummy3.c --output $D3
 strip $D1
 strip $D2
 strip $D3

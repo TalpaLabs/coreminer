fbf683aec6d93694733a3ce9bb3cce71d727c45d
Christoph J. Scherr <software@cscherr.de>
2025-03-05 14:17:13 +0100

    feat(basicui): update the help menu and set loglevel to info

--- src/bins/cm.rs
+++ src/bins/cm.rs
@@ -5,18 +5,38 @@ use coreminer::errors::DebuggerError;
 use coreminer::ui::cli::CliUi;
 
 use clap::Parser;
-use tracing::debug;
+use tracing::trace;
 
-/// Launch the core debugger
+/// Coreminer - A powerful low-level debugger for Linux
+///
+/// Coreminer is designed to debug ELF binaries that may be resistant to standard
+/// debugging approaches. It provides a feature-rich command-line interface with
+/// capabilities including memory inspection, breakpoint management, register manipulation,
+/// and DWARF debug symbol resolution.
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(
+    author,
+    version,
+    about,
+    long_about,
+    help_template = r#"{about-section}
+{usage-heading} {usage}
+{all-args}{tab}
+
+{name}: v{version}
+Authors: {author-with-newline}
+"#
+)]
 struct Args {
+    /// Optional path to the executable to debug with the run command
+    ///
+    /// If provided, this executable will be loaded automatically
+    /// and can be run with the 'run' command without arguments.
     default_executable: Option<PathBuf>,
 }
 
 fn main() -> Result<(), DebuggerError> {
     setup_logger();
-    debug!("set up the logger");
 
     let args = Args::parse();
 
@@ -31,9 +51,10 @@ fn main() -> Result<(), DebuggerError> {
 fn setup_logger() {
     // construct a subscriber that prints formatted traces to stdout
     let subscriber = tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::TRACE)
+        .with_max_level(tracing::Level::INFO)
         .without_time()
         .finish();
     // use that subscriber to process traces emitted after this point
     tracing::subscriber::set_global_default(subscriber).expect("could not setup logger");
+    trace!("set up the logger");
 }

185fb7584340aa48da2596fa6c99f905491508f5
Christoph J. Scherr <software@cscherr.de>
2025-03-06 12:17:48 +0100

    chore: update dummy compile scripts

--- build-dummy.sh
+++ build-dummy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-mkdir -p ./target/debug
-gcc -g ./examples/dummy.c --output ./target/debug/dummy
-gcc -g ./examples/dummy2.c --output ./target/debug/dummy2
-gcc -g ./examples/dummy3.c --output ./target/debug/dummy3
+mkdir -p ./target/release
+DUMMYS=( "dummy" "dummy2" "dummy3" "how_many_fds" )
+
+mkdir -p ./target/debug/
+for DUMMY in ${DUMMYS[*]}; do
+	BINPATH=./target/debug/${DUMMY}
+	gcc -g ./examples/${DUMMY}.c --output $BINPATH
+	echo $BINPATH
+done
--- build-release.sh
+++ build-release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p ./target/release
-D1=./target/release/dummy
-D2=./target/release/dummy2
-D3=./target/release/dummy3
-gcc ./examples/dummy.c --output $D1
-gcc ./examples/dummy2.c --output $D2
-gcc ./examples/dummy3.c --output $D3
-strip $D1
-strip $D2
-strip $D3
+DUMMYS=( "dummy" "dummy2" "dummy3" "how_many_fds" )
+
+mkdir -p ./target/release/
+for DUMMY in ${DUMMYS[*]}; do
+	BINPATH=./target/release/${DUMMY}
+	gcc ./examples/${DUMMY}.c --output $BINPATH
+	strip $BINPATH
+	echo $BINPATH
+done

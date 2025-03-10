cfcc8349b6e74e813b011326e2d18c8b5cd9bf16
Christoph J. Scherr <software@cscherr.de>
2025-03-10 15:00:19 +0100

    feat(plugins): add EOnSigTrap

--- src/extension_points.rs
+++ src/extension_points.rs
@@ -3,8 +3,16 @@ use steckrs::extension_point;
 use nix::libc::siginfo_t;
 use nix::sys::signal::Signal;
 
+use crate::errors::Result;
+
 extension_point!(
-    PreSignalHandler: PreSignalHandlerF,
+    EPreSignalHandler: EPreSignalHandlerF,
     // true if the signal should be ignored
     fn pre_handle_signal(&self, siginfo: &siginfo_t, sig: &Signal) -> bool,
+);
+
+extension_point!(
+    EOnSigTrap: EOnSigTrapF,
+    // true if the signal should be ignored
+    fn handle_sigtrap(&self, siginfo: &siginfo_t, sig: &Signal) -> Result<()>,
 );

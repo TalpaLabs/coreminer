a4d24efd4f93395d53df25fc771fec44d7d87433
Christoph J. Scherr <software@cscherr.de>
2025-03-11 17:44:30 +0100

    chore: add comments and disable missing docs for extension_points module for now

--- src/extension_points.rs
+++ src/extension_points.rs
@@ -1,3 +1,6 @@
+#![allow(missing_docs)] // TODO: add proper docs
+#![allow(clippy::missing_errors_doc)]
+
 use steckrs::extension_point;
 
 use nix::libc::siginfo_t;
@@ -32,7 +35,15 @@ macro_rules! for_hooks {
 }
 
 extension_point!(
-    EPreSignalHandler: EPreSignalHandlerF;
-    /// Will be called in a feedback loop where you can issue regular debugger commands.
+    // [ExtensionPoint](steckrs::hook::ExtensionPoint) called before signals to the debuggee are
+    // handled
+    EPreSignalHandler:
+    // Functions implemented by [EPreSignalHandler]
+    EPreSignalHandlerF;
+    // Will be called in a feedback loop where you can issue regular debugger commands.
+    //
+    // # Errors
+    //
+    // Will error when the implementing plugin somehow fails
     fn pre_handle_signal(&self, feedback: &Feedback, siginfo: &siginfo_t, sig: &Signal) -> Result<Status>;
 );
